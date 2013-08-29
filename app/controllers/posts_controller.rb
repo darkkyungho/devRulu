5f9ab182ba74809ae44ddc8a56e8b7068e032094
Junheon Lee <junheon@outlook.com>
2013-08-29 21:27:49 +0900

    add tags route to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!, except: [:index, :show, :tags]
 
   # GET /posts
   # GET /posts.json
@@ -60,6 +60,18 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to posts_url }
       format.json { head :no_content }
+    end
+  end
+
+  # GET /posts/tags
+  def tags
+    @tags = Post.tag_counts
+
+    @tags = @tags.where('name LIKE ?', "%#{params[:q]}%") if params[:q]
+    @tags = @tags.limit(10)
+
+    respond_to do |format|
+      format.json { render json: @tags}
     end
   end
 
